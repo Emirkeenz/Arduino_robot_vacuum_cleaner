891f15d9c2a5723fe931ce6c9dd3763855e80807
Emirkeenz <orozbekovjob@gmail.com>
2024-12-23 01:26:09 +0600

    turn left and right

--- ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
+++ ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
@@ -17,3 +17,22 @@ void moveForward() {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
 }
+
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  delay(500); // Вращение в течение 0.5 секунды
+  moveStop();
+}
+
+void turnLeft() {
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  delay(500); // Вращение в течение 0.5 секунды
+  moveStop();
+}
+
+void moveStop() {
+  motor1.run(RELEASE); // Остановка моторов
+  motor2.run(RELEASE);
+}
