8b9a66495741aa3612f68c5cb00bc27ff4de3836
Emirkeenz <orozbekovjob@gmail.com>
2024-12-23 01:32:51 +0600

    fixed bugs

--- ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
+++ ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
@@ -1,99 +1,132 @@
-#include <Servo.h>
-#include <NewPing.h>
-
-#define TRIG_PIN A0
-#define ECHO_PIN A1
-#define MAX_DISTANCE 200
-
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor fanMotor(3, MOTOR12_1KHZ);
-
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
-Servo myservo;
-
-void setup() {
-  myservo.attach(10);
-  myservo.write(115); // Начальная позиция серво мотора
-  delay(2000);
-
-  motor1.setSpeed(100);
-  motor2.setSpeed(100);
-  fanMotor.setSpeed(255); // Максимальная скорость
-  fanMotor.run(FORWARD);
-}
-
-void loop() {
-  int distanceR = lookRight();
-  delay(200);
-  int distanceL = lookLeft();
-  delay(200);
-
-  if (distanceR >= distanceL) {
-    moveBackward();
-    delay(300);
-    turnRight();
-  } else {
-    moveBackward();
-    delay(300);
-    turnLeft();
-  }
-}
-
-int lookRight() {
-  myservo.write(50); // Повернуть вправо
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  myservo.write(115); // Вернуть в исходное положение
-  return distance;
-}
-
-int lookLeft() {
-  myservo.write(170); // Повернуть влево
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  myservo.write(115); // Вернуть в исходное положение
-  return distance;
-}
-
-int readPing() {
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm == 0) {
-    cm = 250; // Максимальное расстояние
-  }
-  return cm;
-}
-
-void moveBackward() {
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  delay(300); // Движение назад в течение 0.3 секунды
-  moveStop();
-}
-
-void moveForward() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-}
-
-void turnRight() {
-  motor1.run(FORWARD);
-  motor2.run(BACKWARD);
-  delay(500); // Вращение в течение 0.5 секунды
-  moveStop();
-}
-
-void turnLeft() {
-  motor1.run(BACKWARD);
-  motor2.run(FORWARD);
-  delay(500); // Вращение в течение 0.5 секунды
-  moveStop();
-}
-
-void moveStop() {
-  motor1.run(RELEASE); // Остановка моторов
-  motor2.run(RELEASE);
+#include <AFMotor.h>   
+#include <NewPing.h> 
+#include <Servo.h>  
+ 
+#define TRIG_PIN A0  
+#define ECHO_PIN A1  
+#define MAX_DISTANCE 200  
+#define MAX_SPEED 190 // sets speed of DC motors 
+#define MAX_SPEED_OFFSET 20 
+ 
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);  
+ 
+AF_DCMotor motor1(1, MOTOR12_1KHZ);  
+AF_DCMotor motor2(2, MOTOR12_1KHZ); 
+AF_DCMotor fanMotor(3, MOTOR12_1KHZ); 
+Servo myservo; 
+ 
+void setup() { 
+  myservo.attach(10);   
+  myservo.write(115);  
+  delay(2000); 
+   
+  // Запуск моторов M1 и M2 на движение вперед 
+  motor1.setSpeed(100); // Установка скорости (можно изменить) 
+  motor2.setSpeed(100); // Установка скорости (можно изменить) 
+  motor1.run(FORWARD);  // Вращение вперед 
+  motor2.run(FORWARD);  // Вращение вперед 
+ 
+  // Включение вентилятора на M3 
+  fanMotor.setSpeed(255); // Максимальная скорость для 12 В 
+  fanMotor.run(FORWARD); 
+} 
+ 
+void loop() { 
+  int distanceR = 0; 
+  int distanceL = 0; 
+  delay(40); 
+  
+  if (distance <= 15) { 
+    moveStop(); 
+    delay(100); 
+    moveBackward(); 
+    delay(300); 
+    moveStop(); 
+    delay(200); 
+    distanceR = lookRight(); 
+    delay(200); 
+    distanceL = lookLeft(); 
+    delay(200); 
+ 
+    if (distanceR >= distanceL) { 
+      turnRight(); 
+      moveStop(); 
+    } else { 
+      turnLeft(); 
+      moveStop(); 
+    } 
+  } else { 
+    moveForward(); 
+  } 
+  distance = readPing(); 
+} 
+ 
+int lookRight() { 
+  myservo.write(50);  
+  delay(500); 
+  int distance = readPing(); 
+  delay(100); 
+  myservo.write(115);  
+  return distance; 
+} 
+ 
+int lookLeft() { 
+  myservo.write(170);  
+  delay(500); 
+  int distance = readPing(); 
+  delay(100); 
+  myservo.write(115);  
+  return distance; 
+} 
+ 
+int readPing() {  
+  delay(70); 
+  int cm = sonar.ping_cm(); 
+  if (cm == 0) { 
+    cm = 250; 
+  } 
+  return cm; 
+} 
+ 
+void moveStop() { 
+  motor1.run(RELEASE);  
+  motor2.run(RELEASE); 
+} 
+ 
+void moveForward() { 
+  if (!goesForward) { 
+    goesForward = true; 
+    motor1.run(FORWARD);       
+    motor2.run(FORWARD); 
+    for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) { 
+      motor1.setSpeed(speedSet); 
+      motor2.setSpeed(speedSet); 
+      delay(5); 
+    } 
+  } 
+} 
+ 
+void moveBackward() { 
+  goesForward = false; 
+  motor1.run(BACKWARD);       
+  motor2.run(BACKWARD); 
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) { 
+    motor1.setSpeed(speedSet); 
+    motor2.setSpeed(speedSet); 
+    delay(5); 
+  } 
+} 
+ 
+void turnRight() { 
+  motor1.run(FORWARD); 
+  motor2.run(BACKWARD); 
+  delay(500); 
+  moveStop(); 
+} 
+ 
+void turnLeft() { 
+  motor1.run(BACKWARD);      
+  motor2.run(FORWARD); 
+  delay(500); 
+  moveStop(); 
 }
