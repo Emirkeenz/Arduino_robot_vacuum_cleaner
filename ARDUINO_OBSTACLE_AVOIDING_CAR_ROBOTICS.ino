0f651f4e2e2f4e746b6ad2d7a25856fbd21c1c96
Emirkeenz <orozbekovjob@gmail.com>
2024-12-23 01:30:41 +0600

    read distance and move there where the distance is more

--- ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
+++ ARDUINO_OBSTACLE_AVOIDING_CAR_ROBOTICS.ino
@@ -1,16 +1,74 @@
-#include <AFMotor.h>
+#include <Servo.h>
+#include <NewPing.h>
+
+#define TRIG_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 200
 
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+Servo myservo;
+
 void setup() {
+  myservo.attach(10);
+  myservo.write(115); // Начальная позиция серво мотора
+  delay(2000);
+
   motor1.setSpeed(100);
   motor2.setSpeed(100);
 }
 
 void loop() {
-  moveForward();
-  delay(1000); // Двигаться вперед в течение 1 секунды
+  int distanceR = lookRight();
+  delay(200);
+  int distanceL = lookLeft();
+  delay(200);
+
+  if (distanceR >= distanceL) {
+    moveBackward();
+    delay(300);
+    turnRight();
+  } else {
+    moveBackward();
+    delay(300);
+    turnLeft();
+  }
+}
+
+int lookRight() {
+  myservo.write(50); // Повернуть вправо
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(115); // Вернуть в исходное положение
+  return distance;
+}
+
+int lookLeft() {
+  myservo.write(170); // Повернуть влево
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(115); // Вернуть в исходное положение
+  return distance;
+}
+
+int readPing() {
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm == 0) {
+    cm = 250; // Максимальное расстояние
+  }
+  return cm;
+}
+
+void moveBackward() {
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  delay(300); // Движение назад в течение 0.3 секунды
+  moveStop();
 }
 
 void moveForward() {
